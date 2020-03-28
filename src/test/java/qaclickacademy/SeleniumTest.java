2784253ca6595205b0e51a1d7ffb55dc783dadbe
eyup83 <eyup83@gmail.com>
2020-03-27 20:18:27 -0700

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,25 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testng xml file from maven
+	//how to execute all test cases from maven: mvn test
+	
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+	
+	
+	
+	
+}
